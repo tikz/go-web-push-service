2137d25c24778dafe662ee618926b76a23b576db
Mauro Song <tik@lag.party>
2019-08-11 16:41:34 -0300

    Add return to sw

--- static/sw.js
+++ static/sw.js
@@ -3,7 +3,7 @@
 self.addEventListener('push', function(event) {
     console.log('[Service Worker] Push Received.');
     const payload = event.data ? event.data.json() : 'no payload';
-    self.registration.showNotification(payload.title, {
+    return self.registration.showNotification(payload.title, {
         body: payload.body,
         icon: payload.icon,
     })
